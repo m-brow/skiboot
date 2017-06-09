97918d3b1aa5df1506f28967b3beb196a594a9e3
Matt Brown <matthew.brown.dev@gmail.com>
2017-06-09 16:39:22 +1000

    platform: Add load_done to Habanero platform
    This adds the load_done function to the Habanero platform.
    Allowing Habanero machines to asyncronously decode the initramfs
    during boot.
    
    Signed-off-by: Matt Brown <matthew.brown.dev@gmail.com>

--- platforms/astbmc/habanero.c
+++ platforms/astbmc/habanero.c
@@ -147,6 +147,7 @@ DECLARE_PLATFORM(habanero) = {
 	.elog_commit		= ipmi_elog_commit,
 	.start_preload_resource	= flash_start_preload_resource,
 	.resource_loaded	= flash_resource_loaded,
+	.load_done		= flash_load_boot_resource,
 	.exit			= ipmi_wdt_final_reset,
 	.terminate		= ipmi_terminate,
 };
